fe58af0c84239bb4871af22782d2e05f772ccf27
YRMOX <74454927+YRMOX@users.noreply.github.com>
2021-02-17 10:14:54 +0100

    Update Car.cpp

--- libs/src/Car.cpp
+++ libs/src/Car.cpp
@@ -16,9 +16,9 @@ Car::Car(const char* Brand, const char* Model, float ZeroToHundred, float* gearR
   wheel.setTireReference(tire);
 
   acceleration.setAll(0);
-	speed.setAll(0);
-	position.setAll(0);
-	loger.construct(">>Car Construction<<");
+  speed.setAll(0);
+  position.setAll(0);
+  loger.construct(">>Car Construction<<");
 }
 
 Car::~Car(){
@@ -26,9 +26,16 @@ Car::~Car(){
 }
 
 void Car::setProperties(const char* Brand, const char* Model, float ZeroToHundred, float* gearRatioAdress, float DifRatio, float* engineTorqueAdress, int Mass, const char* tire){
+  brand = Brand;
+  model = Model;
+  zeroToHundred = ZeroToHundred;
+  gearbox.setProperties("NA6A", gearRatioAdress);
+  difRatio = DifRatio;
+  wheel.setTireReference(tire);
+
   acceleration.setAll(0);
-	speed.setAll(0);
-	position.setAll(0);
+  speed.setAll(0);
+  position.setAll(0);
 }
 
 void Car::update(){
