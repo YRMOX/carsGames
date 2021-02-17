0667fc645c0a3a5a09208d13aaa3208ef4b84d42
YRMOX <74454927+YRMOX@users.noreply.github.com>
2021-02-17 10:21:14 +0100

    maj Loger bug correction

--- include/Loger.hpp
+++ include/Loger.hpp
@@ -1,11 +1,16 @@
 #ifndef LOGER_HPP
 #define LOGER_HPP
 
+#include <iostream>
+
+#define cout std::cout
+#define endl std::endl
+
 struct Loger{
     Loger();
     ~Loger();
     template<typename T>
-    void log(T message);
+    void log(T message){ cout << message << endl; }
     void boolLog(bool boolean);
     void vectorLog(float x, float y, float z);
     void construct(const char* message);
